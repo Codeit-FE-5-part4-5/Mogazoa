077a256ac33b1237b56d71196cd5b3f1d02b182d
Kimhansaem <khs_38@naver.com>
2024-07-04 16:16:28 +0900

    초기 세팅 2

---
+++ src/shared/types/es.ts
@@ -0,0 +1,3 @@
+interface a {
+  a: string;
+}
---
+++ src/shared/utils/axios.ts
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import { Cookies } from 'react-cookie';
+
+const cookies = new Cookies();
+
+export const apiInstance = axios.create({
+  baseURL: 'https://mogazoa-api.vercel.app/5-5/',
+});
+
+apiInstance.interceptors.request.use((configOrigin) => {
+  const config = configOrigin;
+  if (typeof window !== 'undefined') {
+    const token = cookies.get('token');
+
+    if (config.headers && token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+
+  return config;
+});
+
+apiInstance.interceptors.response.use(
+  (configOrigin) => {
+    const config = configOrigin;
+
+    return config;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.clear();
+      cookies.remove('token', { path: '/' });
+      window.location.href = '/signin';
+    } else {
+      console.log(error.response.status);
+      throw new Error(error.response.status);
+    }
+  },
+);
+
+export const awsApiInstance = axios.create();
+
+awsApiInstance.interceptors.request.use((config) => {
+  if (typeof window === 'undefined') return config;
+  config.headers.Authorization = null;
+  return config;
+});
