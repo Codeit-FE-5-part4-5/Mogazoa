f75255a22637259c1f9624941a0ac3ffb04200e5
임상훈 <sanghoonim@hanyang.ac.kr>
2024-07-05 14:28:12 +0900

    conflict 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,3 @@
-import EmailInput from '@/shared/components/Input/EmailInput';
-import PasswordInput from '@/shared/components/Input/PasswordInput';
-import NicknameInput from '@/shared/components/Input/NicknameInput';
-import TextFieldInput from '@/shared/components/Input/TextFieldInput';
-import TextAreaInput from '@/shared/components/Input/TextAreaInput';
-import ImageInput from '@/shared/components/Input/ImageInput';
-
 export default function Home() {
-  return (
-    <div className="bg-var-black1 p-10">
-      <EmailInput placeholder="이메일을 입력해주세요." />
-      <PasswordInput placeholder="비밀번호를 입력해주세요." />
-      <NicknameInput placeholder="닉네임을 입력해주세요." />
-      <TextFieldInput placeholder="이름을 입력해주세요." />
-      <TextAreaInput placeholder="자기소개를 입력해주세요." />
-      <ImageInput />
-    </div>
-  );
+  return <div></div>;
 }
