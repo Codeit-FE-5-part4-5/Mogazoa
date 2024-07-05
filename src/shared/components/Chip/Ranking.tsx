ee578a2d3f2b702a36448182ab4d3545bf100c7d
kidboi666 <kidboi666@gmail.com>
2024-07-05 16:22:25 +0900

    RankingChip 로직 구현 완료

--- src/shared/components/Chip/Ranking.tsx
+++ src/shared/components/Chip/Ranking.tsx
@@ -7,17 +7,17 @@ interface RankingProps {
 const chipColorList = ['#ff2f9f', '#05d58b', '#9fa6b2'];
 
 export default function Ranking({ ranking }: RankingProps) {
-  const { r, g, b } = hexToRgb(chipColorList[ranking - 1]);
+  const { r, g, b } = hexToRgb(chipColorList[ranking < 2 ? ranking : 2]);
 
   return (
     <div
       className={`flex w-fit rounded-[16px] bg-var-gray2 px-[6px] py-[2px] md:px-[8px]`}
       style={{
         backgroundColor: `rgba(${r}, ${g}, ${b}, 0.18)`,
-        color: chipColorList[ranking - 1],
+        color: chipColorList[ranking] || chipColorList[2],
       }}
     >
-      <span className="text-[10px] md:text-[12px]">{ranking}등</span>
+      <span className="text-[10px] md:text-[12px]">{ranking + 1}등</span>
     </div>
   );
 }
