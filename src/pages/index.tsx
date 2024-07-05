81663b606bf5456934e8d425b174cfd6a37c7ae6
임상훈 <sanghoonim@hanyang.ac.kr>
2024-07-05 13:08:11 +0900

    [chore] index.tsx에서 컴포넌트 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,19 +6,5 @@ import TextAreaInput from '@/shared/components/Input/TextAreaInput';
 import ImageInput from '@/shared/components/Input/ImageInput';
 
 export default function Home() {
-  return (
-    <div className="bg-var-black1 p-10">
-      <EmailInput placeholder="이메일을 입력해주세요"></EmailInput>
-      <br />
-      <PasswordInput placeholder="비밀번호를 입력해주세요"></PasswordInput>
-      <br />
-      <NicknameInput placeholder="닉네임을 입력해주세요(최대10자)"></NicknameInput>
-      <br></br>
-      <TextFieldInput placeholder="상품명"></TextFieldInput>
-      <br></br>
-      <TextAreaInput placeholder="리뷰를 작성해주세요"></TextAreaInput>
-      <br></br>
-      <ImageInput> </ImageInput>
-    </div>
-  );
+  return <div className="bg-var-black1 p-10"></div>;
 }
