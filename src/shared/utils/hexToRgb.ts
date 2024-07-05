5766603ae447b3fafb2ff86788acb3ec051b9e12
kidboi666 <kidboi666@gmail.com>
2024-07-05 15:10:56 +0900

    Chip 구현 완료

---
+++ src/shared/utils/hexToRgb.ts
@@ -0,0 +1,14 @@
+/**
+ *
+ * @param hexString rgb로 변환시킬 헥스값 문자열
+ * @returns
+ */
+export const hexToRgb = (hexString: string) => {
+  const hex = hexString?.replace('#', '');
+
+  const r = parseInt(hex?.substring(0, 2), 16);
+  const g = parseInt(hex?.substring(2, 4), 16);
+  const b = parseInt(hex?.substring(4, 6), 16);
+
+  return { r, g, b };
+};
