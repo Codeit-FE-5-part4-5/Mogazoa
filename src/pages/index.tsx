dd82c17a948c1cddb9bea5c69515e661570c1de3
JunYoung <wnsdud96330@gmail.com>
2024-07-06 15:06:10 +0900

    fix : 버그 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,3 @@
-import { RankingList } from '@/shared/components/RankingList/RankingList';
-import { SlideMenu } from '@/shared/components/SlideMenu/SlideMenu';
-import { Header } from '@/shared/components/header/header';
-
 export default function Home() {
-  return (
-    <div>
-      <Header />
-      <div className="flex flex-row overflow-hidden">
-        <div>
-          <SlideMenu />
-        </div>
-        <div className="overflow-x-auto">
-          <RankingList />
-        </div>
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
--- src/shared/components/RankingList/RankingList.tsx
+++ src/shared/components/RankingList/RankingList.tsx
@@ -9,11 +9,11 @@ export const RankingList: React.FC = () => {
     review: 1243,
   };
   return (
-    <div className="flex w-[810px] flex-col gap-[20px] bg-[#1C1C22] text-[#F1F1F5] md:w-[828px] lg:h-full lg:w-[250px]">
-      <div className="text-[14px] font-normal lg:pb-[20px] lg:pl-[34px] lg:pt-[45px]">
+    <div className="flex w-[810px] flex-col gap-[20px] bg-[#1C1C22] text-[#F1F1F5] md:w-[828px] xl:h-full xl:w-[250px]">
+      <div className="text-[14px] font-normal xl:pb-[20px] xl:pl-[34px] xl:pt-[45px]">
         리뷰어 랭킹
       </div>
-      <div className="flex gap-[15px] lg:flex lg:flex-shrink-0 lg:flex-col lg:items-start lg:gap-[30px] lg:px-[30px]">
+      <div className="flex gap-[15px] xl:flex xl:flex-shrink-0 xl:flex-col xl:items-start xl:gap-[30px] xl:px-[30px]">
         <RankingCard mockRanking={mockRanking} />
         <RankingCard mockRanking={mockRanking} />
         <RankingCard mockRanking={mockRanking} />
