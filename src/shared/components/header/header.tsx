0dc7d9489c1b95df5009162e3b167fb0ca4f804d
kidboi666 <kidboi666@gmail.com>
2024-07-08 15:41:35 +0900

    remove: 미사용, 테스트 코드 삭제

--- src/shared/components/header/header.tsx
+++ src/shared/components/header/header.tsx
@@ -5,9 +5,6 @@ import LogoIcon from '@/../../public/images/logo.svg';
 import MenuIcon from '@/../../public/images/menu.svg';
 import searchIcon from '@/../../public/images/search.svg';
 import closedIcon from '@/../../public/images/closedIcon.svg';
-import Button from '../Button/Button';
-import { removeCookie } from '@/shared/utils/cookie';
-import { useRouter } from 'next/router';
 
 interface HeaderProps {
   me?: boolean;
@@ -15,24 +12,21 @@ interface HeaderProps {
 
 export const Header: React.FC<HeaderProps> = ({ me }) => {
   const [isSearchOpen, setIsSearchOpen] = useState(false);
-  const router = useRouter();
+  const [isOpenMenu, setOpenMenu] = useState(false);
 
   const handleSearchClick = () => {
     setIsSearchOpen(!isSearchOpen);
   };
 
-  // 테스트를 위한 함수
-  const logout = () => {
-    removeCookie('accessToken');
-    router.reload();
-  };
-
   return (
     <div className="sticky flex w-full flex-col items-start gap-[10px] bg-[#1C1C22] stroke-[#252530] stroke-[1px] px-[20px] py-[23px] md:px-[30px] xl:px-[120px]">
       <div className="flex w-full items-center justify-between">
-        <div className="flex items-center space-x-4 md:hidden">
+        <button
+          onClick={() => setOpenMenu((prev) => !prev)}
+          className="flex cursor-pointer items-center space-x-4 md:hidden"
+        >
           <Image src={MenuIcon} alt="MenuIcon" width={24} height={24} />
-        </div>
+        </button>
         <div className="flex items-center space-x-4">
           <Link href="/">
             <Image src={LogoIcon} alt="LogoIcon" width={166} height={28} />
