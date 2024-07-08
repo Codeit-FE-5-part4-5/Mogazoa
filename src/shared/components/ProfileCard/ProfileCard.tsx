2be3fdaa563de6375c5fae69775ba4829f2ada84
Homin <coc1381@naver.com>
2024-07-08 16:36:20 +0900

    feat: ProfileCard 컴포넌트에서 팔로워 / 팔로잉 부분을 누르면 모달이 뜨도록 하는 기능 추가

--- src/shared/components/ProfileCard/ProfileCard.tsx
+++ src/shared/components/ProfileCard/ProfileCard.tsx
@@ -1,7 +1,10 @@
 import Image from 'next/image';
 import Button from '../Button/Button';
+import { useModal } from '@/shared/hooks/use-modal-store';
 
 export default function ProfileCard() {
+  const { onOpen } = useModal();
+
   return (
     <div className="xl: flex flex-col items-center gap-[30px] rounded-[12px] border border-var-black3 bg-var-black2 px-[20px] py-[30px] text-[14px] text-var-white md:px-[30px] xl:gap-[40px] xl:px-[20px] xl:pt-[40px]">
       <Image
@@ -17,11 +20,17 @@ export default function ProfileCard() {
         프로쇼핑러! 안녕하세요, 별점의 화신 surisuri마수리입니다!
       </p>
       <div className="flex w-full">
-        <div className="flex flex-1 flex-col items-center border-r border-var-black3">
+        <div
+          className="flex flex-1 cursor-pointer flex-col items-center border-r border-var-black3"
+          onClick={() => onOpen('follow')}
+        >
           <span className="text-[18px]">762</span>
           <span>팔로워</span>
         </div>
-        <div className="flex flex-1 flex-col items-center">
+        <div
+          className="flex flex-1 cursor-pointer flex-col items-center"
+          onClick={() => onOpen('follow')}
+        >
           <span className="text-[18px]">102</span>
           <span>팔로잉</span>
         </div>
