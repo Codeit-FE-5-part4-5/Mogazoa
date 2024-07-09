8bbd5863251bcbcba4c89e4f8934ac9ab1cf25b0
JunYoung <wnsdud96330@gmail.com>
2024-07-09 13:14:14 +0900

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,11 @@
+import { RankingList } from '@/shared/components/RankingList/RankingList';
+import { SlideMenu } from '@/shared/components/SlideMenu/SlideMenu';
+import { Header } from '@/shared/components/header/header';
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Header /> <SlideMenu /> <RankingList />
+    </div>
+  );
 }
