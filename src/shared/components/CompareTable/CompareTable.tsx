47ad0069f9dc0e005fbe40dc4b2f1d337127e023
Kimhansaem <khs_38@naver.com>
2024-07-15 20:32:58 +0900

    fix: Table > CompareTable 컴포넌트 명 변경

---
+++ src/shared/components/CompareTable/CompareTable.tsx
@@ -0,0 +1,54 @@
+import { compareProducts } from '@/shared/models/product/compareProducts';
+import { Product } from '@/shared/types/product/product';
+
+interface Props {
+  integratedData: {
+    product2?: Product | undefined;
+    product1?: Product | undefined;
+  };
+}
+
+export const CompareTable = ({ integratedData }: Props) => {
+  const { product1, product2 } = integratedData;
+
+  return (
+    <div className="rounded-[12px] border border-solid border-[#353542] bg-[rgb(37,37,48)] text-center text-[12px] md:text-[14px] xl:text-[16px]">
+      <ul className="grid grid-cols-4 border-b border-solid border-b-[#353542] py-[15px] text-var-gray2 md:py-[20px]">
+        <li>기준</li>
+        <li>상품 1</li>
+        <li>상품 2</li>
+        <li>결과</li>
+      </ul>
+      <ul className="grid grid-cols-4 py-[15px] md:py-[30px]">
+        <li className="text-var-gray2">별점</li>
+        <li>{product1?.rating ?? '-'}</li>
+        <li>{product2?.rating ?? '-'}</li>
+        <li className="text-var-pink">
+          {compareProducts(product1?.rating || 0, product2?.rating || 0)}
+        </li>
+      </ul>
+      <ul className="grid grid-cols-4 py-[15px] md:py-[30px]">
+        <li className="text-var-gray2">리뷰 개수</li>
+        <li>{product1?.reviewCount ?? '-'}</li>
+        <li>{product2?.reviewCount ?? '-'}</li>
+        <li className="text-var-pink">
+          {compareProducts(
+            product1?.reviewCount || 0,
+            product2?.reviewCount || 0,
+          )}
+        </li>
+      </ul>
+      <ul className="grid grid-cols-4 py-[15px] md:py-[30px]">
+        <li className="text-var-gray2">찜 개수</li>
+        <li>{product1?.favoriteCount ?? '-'}</li>
+        <li>{product2?.favoriteCount ?? '-'}</li>
+        <li className="text-var-pink">
+          {compareProducts(
+            product1?.favoriteCount || 0,
+            product2?.favoriteCount || 0,
+          )}
+        </li>
+      </ul>
+    </div>
+  );
+};
