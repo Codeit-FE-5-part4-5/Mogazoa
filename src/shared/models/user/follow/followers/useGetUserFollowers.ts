3569065cc57580a4386f295c366aa7f798a503fb
Homin <coc1381@naver.com>
2024-07-15 18:15:19 +0900

    feat: follwee modal 무한스크롤 구현 완료

--- src/shared/models/user/follow/followers/useGetUserFollowers.ts
+++ src/shared/models/user/follow/followers/useGetUserFollowers.ts
@@ -1,5 +1,5 @@
 import axios from '@/shared/utils/axios';
-import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 
 const useGetUserFollowers = (userId: number | undefined) => {
   return useInfiniteQuery({
