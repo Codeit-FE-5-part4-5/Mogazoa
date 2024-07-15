ec3a76f994c72d542de1068c21eb88c06c43f018
Kimhansaem <khs_38@naver.com>
2024-07-15 20:31:39 +0900

    feat: CompareResult 컴포넌트 추가

---
+++ src/shared/components/CompareResult/CompareResult.tsx
@@ -0,0 +1,14 @@
+export const CompareResult = () => {
+  return (
+    <div className="text-center">
+      <span className="text-[20px] font-semibold leading-[28px] text-var-white xl:text-[24px] xl:leading-normal">
+        <span className="text-var-pink">Air Pods Max</span> 상품이
+        <br className="md:inline-block md:px-[2px] md:content-['']" />
+        승리하였습니다!
+      </span>
+      <div className="mt-[20px] text-[12px] font-normal text-var-gray2 xl:text-[16px]">
+        3가지 항목 중 2가지 항목에서 우세합니다.
+      </div>
+    </div>
+  );
+};
