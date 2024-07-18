83f8c4d4b2052db42726c756e0a520e21e1e6fd7
kidboi666 <kidboi666@gmail.com>
2024-07-18 12:33:39 +0900

    refactor: 스피너 로직 수정

---
+++ src/shared/hooks/usePendingTimeout.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+
+const usePendingTimeout = (isLoading: boolean) => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    let timerId = setTimeout(() => {
+      if (isLoading) {
+        setLoading(true);
+      } else {
+        setLoading(false);
+      }
+    }, 400);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [isLoading]);
+
+  return [loading];
+};
+
+export default usePendingTimeout;
