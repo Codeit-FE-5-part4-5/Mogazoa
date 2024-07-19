ba0a7c0d346e4f491e7803b27e3f532edfee157b
kidboi666 <kidboi666@gmail.com>
2024-07-19 16:09:09 +0900

    chore: react-error-boundary 라이브러리 설치, feat: FetchBoundary컴포넌트 추가, refactor: 함수 컴포넌트 선언방식 수정

--- src/shared/models/category/useGetCategory.ts
+++ src/shared/models/category/useGetCategory.ts
@@ -1,7 +1,7 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useSuspenseQuery } from '@tanstack/react-query';
 import axios from '@/shared/utils/axios';
 
-export default function useGetCategory() {
+const useGetCategory = () => {
   return useQuery({
     queryKey: ['category'],
     queryFn: async () => {
@@ -9,4 +9,6 @@ export default function useGetCategory() {
       return data;
     },
   });
-}
+};
+
+export default useGetCategory;
