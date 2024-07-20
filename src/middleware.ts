cccf3a0485cf7771cff13dfc8ef26a65effe01f5
kidboi666 <kidboi666@gmail.com>
2024-07-20 18:06:54 +0900

    rename: middleware 함수명 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,26 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-const redirectIfLoggedIn = (req: NextRequest) => {
+const checkLogin = (req: NextRequest) => {
   const token = req.cookies.get('accessToken');
+  return token;
+};
 
-  if (token) {
+const redirectIfLoggedIn = (req: NextRequest) => {
+  const redirectedPaths = [
+    '/signup',
+    '/signin',
+    '/oauth/kakao',
+    '/oauth/google',
+  ];
+
+  if (checkLogin(req) && redirectedPaths.includes(req.nextUrl.pathname)) {
     return NextResponse.redirect(new URL('/', req.url));
   }
   return NextResponse.next();
 };
 
-const restrictPath = (req: NextRequest) => {
+const redirectIfNotLoggedIn = (req: NextRequest) => {
   const restrictedPaths = ['/mypage', '/compare'];
 
   if (restrictedPaths.includes(req.nextUrl.pathname)) {
@@ -23,12 +33,13 @@ const middleware = (req: NextRequest) => {
   let response = redirectIfLoggedIn(req);
 
   if (response?.status === 200) {
-    response = restrictPath(req);
+    response = redirectIfNotLoggedIn(req);
   }
   return response;
 };
 
 export default middleware;
+
 export const config = {
   matcher: ['/signin', '/signup', '/oauth/:path*', '/mypage', '/compare'],
 };
