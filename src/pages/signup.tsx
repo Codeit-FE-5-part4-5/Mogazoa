92154defd8a6fc3aac3c774b0d51a75c0c153689
kidboi666 <kidboi666@gmail.com>
2024-07-20 18:17:37 +0900

    feat: 회원가입 유효성 검증 스키마 추가

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -26,9 +26,21 @@ const signUpSchema = z
       .string()
       .min(1, '닉네임을 입력해 주세요.')
       .max(10, '닉네임은 최대 10자 까지 가능합니다.'),
-    password: z.string().min(8, '비밀번호는 최소 8자 이상이어야 합니다.'),
+    password: z
+      .string()
+      .regex(
+        /^([a-z]|[A-Z]|[0-9]|[!@#$%^&*])+$/,
+        '비밀번호는 숫자,영문,특수문자로만 가능합니다.',
+      )
+      .min(1, '비밀번호는 필수 입력입니다.')
+      .min(8, '비밀번호는 최소 8자 이상이어야 합니다.'),
     passwordConfirmation: z
       .string()
+      .regex(
+        /^([a-z]|[A-Z]|[0-9]|[!@#$%^&*])+$/,
+        '비밀번호는 숫자,영문,특수문자로만 가능합니다.',
+      )
+      .min(1, '비밀번호 확인을 입력해주세요.')
       .min(8, '비밀번호는 최소 8자 이상이어야 합니다.'),
   })
   .refine((data) => data.password === data.passwordConfirmation, {
