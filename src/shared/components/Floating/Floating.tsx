5a13ade4731accc7baa426eb8db87b0e252a334c
kidboi666 <kidboi666@gmail.com>
2024-07-20 19:48:42 +0900

    feat: floating 로그인시에만 렌더링

--- src/shared/components/Floating/Floating.tsx
+++ src/shared/components/Floating/Floating.tsx
@@ -1,17 +1,20 @@
+import useMe from '@/shared/store/use-me';
 import { useModal } from '@/shared/store/use-modal-store';
 import Image from 'next/image';
 
 const Floating = () => {
   const { onOpen } = useModal();
-  const handleClick = () => {
-    onOpen('itemAdd');
-  };
+  const { isLoggedIn } = useMe();
+
+  if (!isLoggedIn) {
+    return null;
+  }
 
   return (
     <div className="fixed bottom-[10%] right-[10%] z-40">
       <button
         className="flex size-[60px] items-center justify-center rounded-[50%] bg-gradient-custom shadow-xl"
-        onClick={handleClick}
+        onClick={() => onOpen('itemAdd')}
       >
         <Image src="/plus.svg" alt="플러스 버튼" width={40} height={40} />
       </button>
