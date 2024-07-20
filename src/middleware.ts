02f1f6babbc3d61ce33c2d48f97c0ebac4606cd1
kidboi666 <kidboi666@gmail.com>
2024-07-20 17:01:55 +0900

    feat: 로그인시, 미로그인시 리다이렉션 미들웨어로 구현

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,34 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export function middleware(req: NextRequest) {
+const redirectIfLoggedIn = (req: NextRequest) => {
   const token = req.cookies.get('accessToken');
+
   if (token) {
     return NextResponse.redirect(new URL('/', req.url));
   }
-}
+  return NextResponse.next();
+};
 
+const restrictPath = (req: NextRequest) => {
+  const restrictedPaths = ['/mypage', '/compare'];
+
+  if (restrictedPaths.includes(req.nextUrl.pathname)) {
+    return NextResponse.redirect(new URL('/signin', req.url));
+  }
+  return NextResponse.next();
+};
+
+const middleware = (req: NextRequest) => {
+  let response = redirectIfLoggedIn(req);
+
+  if (response?.status === 200) {
+    response = restrictPath(req);
+  }
+  return response;
+};
+
+export default middleware;
 export const config = {
-  matcher: ['/signin', '/signup', '/oauth/:path*'],
+  matcher: ['/signin', '/signup', '/oauth/:path*', '/mypage', '/compare'],
 };
