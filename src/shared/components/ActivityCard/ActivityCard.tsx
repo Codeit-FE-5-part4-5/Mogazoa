30fe252840d8e011f4e61bda57d8073648b46b7b
jose0229 <sanghoonim@hanyang.ac.kr>
2024-07-25 14:27:44 +0900

    test

--- src/shared/components/ActivityCard/ActivityCard.tsx
+++ src/shared/components/ActivityCard/ActivityCard.tsx
@@ -15,7 +15,7 @@ const ActivityCard = ({ status, conScore, text, color }: ActivityCardProps) => {
       imgComponent: (
         <img src="/images/star.svg" alt="" className="h-[14px] xl:h-[22px]" />
       ),
-      conScore: conScore,
+      conScore,
     },
 
     reviewsLeft: {
@@ -27,13 +27,13 @@ const ActivityCard = ({ status, conScore, text, color }: ActivityCardProps) => {
           className="h-[14px] xl:h-[22px]"
         />
       ),
-      conScore: conScore,
+      conScore,
     },
 
     interest: {
       name: '관심 카테고리',
       imgComponent: <Chip text={text ?? ''} color={color} size="s" />,
-      conScore: conScore,
+      conScore,
     },
   };
 
@@ -42,19 +42,17 @@ const ActivityCard = ({ status, conScore, text, color }: ActivityCardProps) => {
   const changeScore = config[status].conScore;
 
   return (
-    <>
-      <div className="flex h-full flex-col justify-center rounded-[12px] border border-[#353542] bg-[#252530] px-5 py-5 md:justify-end md:rounded-[8px] md:py-[30px] md:text-center xl:rounded-[12px]">
-        <div className="items-center gap-[5px] md:flex md:flex-col md:gap-0">
-          <div className="mb-[15px] text-center text-[14px] text-var-gray2 xl:mb-5 xl:text-[16px]">
-            {changeName}
-          </div>
-          <div className="flex items-center justify-center gap-[5px] text-[16px] font-light text-var-gray2 md:mt-[15px] md:text-[20px] xl:mt-[20px] xl:text-[24px]">
-            {changeImgComponent}
-            {changeScore}
-          </div>
+    <div className="flex h-full flex-col justify-center rounded-[12px] border border-[#353542] bg-[#252530] px-5 py-5 md:justify-end md:rounded-[8px] md:py-[30px] md:text-center xl:rounded-[12px]">
+      <div className="items-center gap-[5px] md:flex md:flex-col md:gap-0">
+        <div className="mb-[15px] text-center text-[14px] text-var-gray2 xl:mb-5 xl:text-[16px]">
+          {changeName}
+        </div>
+        <div className="flex items-center justify-center gap-[5px] text-[16px] font-light text-var-gray2 md:mt-[15px] md:text-[20px] xl:mt-[20px] xl:text-[24px]">
+          {changeImgComponent}
+          {changeScore}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
