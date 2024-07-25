d2e4b129ab9b058fa57f80fe97da99cb5d756f2f
kidboi666 <kidboi666@gmail.com>
2024-07-25 16:58:09 +0900

    fix: 구글 로그인 로직 수정 #7

--- src/pages/api/oauth/google.ts
+++ src/pages/api/oauth/google.ts
@@ -43,15 +43,15 @@ const getIdToken = async (code: string) => {
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { code, state } = req.query;
 
+  let idTokenResponse;
   let idToken;
   let response;
   try {
     if (code) {
-      const {
-        // eslint-disable-next-line @typescript-eslint/naming-convention
-        data: { id_token },
-      } = await getIdToken(validateArray(code));
-      idToken = id_token;
+      idTokenResponse = await getIdToken(validateArray(code));
+      if (idTokenResponse?.status === 200) {
+        idToken = idTokenResponse.data.id_token;
+      }
     }
 
     if (!state) {
@@ -88,3 +88,13 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 };
 
 export default handler;
+
+/**
+ *  구글 로그인 || 회원가입 로직
+ * 1. 구글 인가코드를 받기 위해 리다이렉트 URI와 필요한 값을 넘긴다.
+ * 2. 리다이렉트 URI의 데이터 처리 로직을 실행시킨다.
+ * 3. 가입여부 상관없이 받은 인가코드를 이용해 idToken을 받는다.
+ * 4. idToken으로 로그인 요청을 보내어 가입 여부를 판단한다.
+ * 5. 로그인이 되어 있다면 로그인 처리를 한다.
+ * 6. 로그인이 되어 있지 않다면 가입 처리를 한다.
+ */
