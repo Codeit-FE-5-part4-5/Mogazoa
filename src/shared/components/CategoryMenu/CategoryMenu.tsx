3dc4bca48ee6cc3e1e918f0e12d619d8c4fe6511
kidboi666 <kidboi666@gmail.com>
2024-08-01 12:28:26 +0900

    refactor: 카테고리 메뉴 분기점 else로 감싸줌

--- src/shared/components/CategoryMenu/CategoryMenu.tsx
+++ src/shared/components/CategoryMenu/CategoryMenu.tsx
@@ -28,51 +28,52 @@ const CategoryMenu = ({
         />
       </div>
     );
-  }
-
-  return (
-    <div
-      className={cn(
-        'mx-[20px] hidden w-[160px] flex-shrink-0 flex-col bg-[#1C1C22] text-white md:flex',
-        isVisible ? 'my-[20px]' : 'mt-[45px]',
-      )}
-    >
+    // eslint-disable-next-line no-else-return
+  } else {
+    return (
       <div
         className={cn(
-          'ml-[20px] pb-[15px] text-sm font-normal leading-normal text-white',
-          isVisible && 'hidden',
+          'mx-[20px] w-[160px] flex-shrink-0 flex-col bg-[#1C1C22] text-white',
+          isVisible ? 'my-[20px]' : 'mt-[45px]',
         )}
       >
-        전체 카테고리
+        <div
+          className={cn(
+            'ml-[20px] pb-[15px] text-sm font-normal leading-normal text-white',
+            isVisible && 'hidden',
+          )}
+        >
+          전체 카테고리
+        </div>
+        <ul className="flex flex-col gap-[4px]">
+          {CATEGORY_LIST.map((item: Category) => (
+            <button
+              type="button"
+              key={item.id}
+              className={cn(
+                'flex h-[45px] cursor-pointer items-center rounded-2xl px-[20px] py-[15px] text-sm font-medium leading-normal transition-colors duration-300 hover:text-var-gray2',
+                currentCategoryName === item.name
+                  ? 'border-[#353542] bg-[#252530]'
+                  : 'bg-[#1C1C22] text-[#6E6E82]',
+              )}
+              onClick={() => {
+                if (item.name === currentCategoryName) {
+                  handleClickCategory({});
+                } else {
+                  handleClickCategory({
+                    category: item.name,
+                    categoryId: String(item.id),
+                  });
+                }
+              }}
+            >
+              {item.name}
+            </button>
+          ))}
+        </ul>
       </div>
-      <ul className="flex flex-col gap-[4px]">
-        {CATEGORY_LIST.map((item: Category) => (
-          <button
-            type="button"
-            key={item.id}
-            className={cn(
-              'flex h-[45px] cursor-pointer items-center rounded-2xl px-[20px] py-[15px] text-sm font-medium leading-normal transition-colors duration-300 hover:text-var-gray2',
-              currentCategoryName === item.name
-                ? 'border-[#353542] bg-[#252530]'
-                : 'bg-[#1C1C22] text-[#6E6E82]',
-            )}
-            onClick={() => {
-              if (item.name === currentCategoryName) {
-                handleClickCategory({});
-              } else {
-                handleClickCategory({
-                  category: item.name,
-                  categoryId: String(item.id),
-                });
-              }
-            }}
-          >
-            {item.name}
-          </button>
-        ))}
-      </ul>
-    </div>
-  );
+    );
+  }
 };
 
 export default CategoryMenu;
